f0afbe6c518e77483b0fffed59795f2de5a66341
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-01-31 18:40:27 -0200

    add github home

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Github/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Github from './components/Github'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Github
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/sass/app.scss" lang="sass"></style>
---
+++ src/components/Github.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <header>
+            <div class="row bg-dark text-light">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-3">
+                            <h2>GitHub</h2>
+                        </div>
+                        <div class="col-5">
+                            <!-- <SearchUser updateUser={this.updateUser} updateRepository={this.updateRepository} /> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main class="container mt-3">
+            <!-- <UserInfo user={this.state.user} repositories={this.state.repositories} /> -->
+        </main>
+        <footer class="footer mt-3">
+            <div class="container">
+                <span class="text-muted">{{msg}}</span>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Github',
+    data() {
+        return { 
+            msg: 'Github with VueJs'
+        }
+    }
+};
+</script>
+
+<style>
+.footer {
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #f5f5f5;
+}
+</style>
