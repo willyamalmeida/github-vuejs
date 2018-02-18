67c834af000649eca95d3ff138887458c5323732
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-18 10:36:57 -0300

    travis

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+npm run build
+
+SOURCE_BRANCH="master"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+  echo "Skipping deploy."
+  exit 0
+fi
+
+cd dist
+git init
+
+git config user.name "Travis CI"
+git config user.email "willyamalmeida@hotmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF} " master:gh-pages > /dev/null 2>&1
+
